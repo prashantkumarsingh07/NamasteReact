08f353cbbb754a208f6fb623fd9bac35c3279b08
prashantkumarsingh07 <99783341+prashantkumarsingh07@users.noreply.github.com>
2023-08-04 11:40:46 +0530

    EP 3 part 3

--- App.js
+++ App.js
@@ -1,3 +1,21 @@
-const heading= React.createElement("h1",{},"Hello world from react");
-  const root=ReactDOM.createRoot(document.getElementById("root"));
-  root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+// React.createElement => ReactElement-Js Object=HTMLELEment(render)
+const heading =React.createElement("h1",{id:"heading"},"Namaste React");
+console.log(heading);
+
+// JSX HTML -like or XML -like syntax
+// JSX(transpiled before it reaches the JS engine) - PARCEL - BABEL
+
+// JSX => React.createElement => ReactElement - JS Object => HTMLElement(render)
+
+ const jsxHeading=<h1 id="heading" tabIndex="5">Namaste React using JSX</h1>;
+ 
+ 
+ 
+ const root =ReactDOM.createRoot(document.getElementById("root"));
+
+ root.render(jsxHeading);
+
+
+
